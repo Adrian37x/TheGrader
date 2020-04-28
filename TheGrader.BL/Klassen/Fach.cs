90e23d7ed845370b5aba08dfa6eee21aa6728ddf
Nina <nina.smrekar01@gmail.com>
2020-04-28 09:19:58 +0200

    Klassen hinzugefügt

---
+++ TheGrader.BL/Klassen/Fach.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheGrader.BL.Klassen
+{
+    class Fach
+    { 
+        private string name;
+    }
+}
---
+++ TheGrader.BL/Klassen/LearningTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGrader.BL.Klassen
+{
+    class LearningTime
+    {
+        private DateTime date;
+        private double spentMinutes;
+    }
+}
