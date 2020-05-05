9bf1b47cd11e8b1a722e9eeb742a751053ddff51
Adrian <48199240+Adrian37x@users.noreply.github.com>
2020-05-05 09:33:38 +0200

    exam page list and detail

--- Classes/Exam.cs
+++ Classes/Exam.cs
@@ -9,18 +9,22 @@ namespace TheGrader
     public class Exam
     {
         #region properties
+        public string Name { get; set; }
         public double Grade { get; set; }
         public double PointsScored { get; set; }
         public double MaxPoints { get; set; }
         public double Weight { get; set; }
+        public DateTime Date { get; set; }
         #endregion
 
         #region constructor
-        public Exam(double pointsScored, double maxPoints, double weight, double grade = 0)
+        public Exam(string name, double pointsScored, double maxPoints, double weight, DateTime date, double grade = 0)
         {
+            this.Name = name;
             this.PointsScored = pointsScored;
             this.MaxPoints = maxPoints;
             this.Weight = weight;
+            this.Date = date;
             if (grade != 0){
                 CalculateGrade();
             }
--- Pages/SubjectPage.xaml.cs
+++ Pages/SubjectPage.xaml.cs
@@ -21,13 +21,36 @@ namespace TheGrader.Pages
     /// </summary>
     public partial class SubjectPage : Page
     {
-        //public ObservableCollection<Exam> Moments { get; set; } = new ObservableCollection<Exam>();
+        public ObservableCollection<Exam> Exams { get; set; } = new ObservableCollection<Exam> { 
+            new Exam("File 1", 134, 155, 1, DateTime.Now),
+            new Exam("Pollution Presentation", 14, 20, .3f, DateTime.Now),
+            new Exam("Vocab 3", 29, 30, .5f, DateTime.Now),
+            new Exam("File 2", 120, 148, 1, DateTime.Now),
+        };
+        public Exam SelectedExam { get; set; }
 
         public SubjectPage()
         {
             InitializeComponent();
 
-            CreateEditPanel.DataContext = "öalksdjfasödf";
+            CreateEditPanel.DataContext = "";
+
+            Exams.OrderBy(exam => exam.Date);
+            foreach (Exam exam in Exams)
+            {
+                Button button = new Button();
+                button.Content = exam.Name;
+                button.Background = Brushes.Gray;
+                button.Click += (s, ev) => SelectExam_Click(s, ev, exam);
+                ExamPanel.Children.Add(button);
+            }
+        }
+
+        public void SelectExam_Click(object sender, RoutedEventArgs e, Exam exam)
+        {
+            CreateEditPanel.DataContext = "Edit";
+            SelectedExam = exam;
+            FormPanel.DataContext = SelectedExam;
         }
     }
 }
