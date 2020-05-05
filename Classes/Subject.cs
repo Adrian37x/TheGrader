30dd0203cd556e49c62895023cfe2efb64d6f464
Nina <nina.smrekar01@gmail.com>
2020-05-05 09:05:50 +0200

    Klassen

---
+++ Classes/Subject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheGrader
+{
+    public class Subject  
+    {
+        #region properties
+        public string Name { get; set; }
+        public List<Exam> Exams { get; set; }
+        public double Average { get; set; }
+        #endregion
+
+        #region methods
+        private void CalculateAverage()
+        {
+            double summe = 0;
+            foreach (var exam in Exams){
+                summe = summe + exam.Grade;
+            }
+            Average = summe / Exams.Count;
+        } 
+        #endregion
+    }
+}
